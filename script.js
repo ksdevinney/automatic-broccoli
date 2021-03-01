9a25ed9a3cc7cc293f3286ac66fb8c315dd6cb7c
ksdevinney <katie@devinney.org>
2021-02-28 20:58:47 -0600

    created 3 files, started html and css

---
+++ script.js
@@ -0,0 +1,18 @@
+//timer starts as first question is displayed
+
+//pick an answer
+//if it is right, some sort of notification (answer turns green?)
+
+//if it is wrong, notification (answer turns red?)
+//timer deducts 5 seconds
+
+//game ends when all questions are answered
+//or timer hits zero
+
+//game ends
+
+//score is stored in local storage
+
+//user inputs initials
+
+//score and initials are displayed
