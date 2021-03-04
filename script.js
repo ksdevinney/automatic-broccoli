9b24493cb0d04a379a4d2eb3899f0c10ce3e2f13
ksdevinney <katie@devinney.org>
2021-03-03 19:14:02 -0600

    add questions, link js and css, some stuff works

--- script.js
+++ script.js
@@ -1,4 +1,77 @@
+//variables
+var startButton = document.getElementById("start");
+var submitButton = document.getElementById("submit");
+var quizContainer = document.getElementById("quiz");
+var resultsContainer = document.getElementById('results');
+var timerElement = document.getElementById("timer");
+var questionEl = document.getElementById("question");
+var choiceEl = document.getElementById("choices");
+
+var timer;
+var timerCount;
+
+//questions
+const jsQuestions = [
+    {
+        question: "Choose a number.",
+        answers: {
+            a: "1", 
+            b: "threve" ,
+            c: "hot dog"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Choose a number.",
+        answers: {
+            a: "1", 
+            b: "threve" ,
+            c: "hot dog"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Choose a number.",
+        answers: {
+            a: "1", 
+            b: "threve" ,
+            c: "hot dog"
+        },
+        correctAnswer: "c"
+    }
+]
+
+function loadQuestion () {
+    showQuestion = jsQuestions[Math.floor(Math.random() * jsQuestions.length)];
+
+}
+
+function buildQuiz() {
+    timerCount = 60;
+    loadQuestion();
+}
+
+function startTimer() {
+    timer = setInterval(function() {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) {
+            clearInterval(timer);
+            winGame();
+        }
+    })
+}
+
+//display first question on start
+startButton.addEventListener("click", buildQuiz);
+
 //timer starts as first question is displayed
+
+
+//init is called when the page loads
+function init() {
+    buildQuiz();
+}
 
 //pick an answer
 //if it is right, some sort of notification (answer turns green?)
