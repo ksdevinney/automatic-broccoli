04f0e73f12eca78d1d738015cde25215c1de344e
ksdevinney <katie@devinney.org>
2021-03-03 19:54:15 -0600

    working timer, start and submit buttons work

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ var resultsContainer = document.getElementById('results');
 var timerElement = document.getElementById("timer");
 var questionEl = document.getElementById("question");
 var choiceEl = document.getElementById("choices");
+var timer = document.getElementById("timer");
 
-var timer;
 var timerCount;
+var rightAnswers;
+var userInitials;
 
 //questions
 const jsQuestions = [
@@ -42,25 +44,38 @@ const jsQuestions = [
 ]
 
 function loadQuestion () {
-    showQuestion = jsQuestions[Math.floor(Math.random() * jsQuestions.length)];
+    quizContainer.textContent = jsQuestions[Math.floor(Math.random() * jsQuestions.length)];
 
 }
 
 function buildQuiz() {
-    timerCount = 60;
+    startTimer();
     loadQuestion();
+    timerCount = 10;
 }
 
 function startTimer() {
+    // Sets timer
     timer = setInterval(function() {
-        timerCount--;
-        timerElement.textContent = timerCount;
-        if (timerCount >= 0) {
-            clearInterval(timer);
-            winGame();
-        }
-    })
-}
+      timerCount--;
+      timerElement.textContent = timerCount;
+    //   if (timerCount >= 0) {
+    //     // Tests if win condition is met
+    //     if (isWin && timerCount > 0) {
+    //       // Clears interval and stops timer
+    //       clearInterval(timer);
+    //       winGame();
+    //     }
+    //   }
+    //   // Tests if time has run out
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        alert("Game over!");
+      }
+    }, 1000);
+  }
+  
 
 //display first question on start
 startButton.addEventListener("click", buildQuiz);
@@ -71,6 +86,14 @@ startButton.addEventListener("click", buildQuiz);
 //init is called when the page loads
 function init() {
     buildQuiz();
+}
+
+submitButton.addEventListener("click" , gameEnd);
+
+function gameEnd() {
+    rightAnswers = 3;    
+    userInitials = prompt("Enter your name to save your score!");
+    console.log(userInitials, rightAnswers);
 }
 
 //pick an answer
